b46c941c5c05541d24ea031e9e04d22571856bb3
qwer1304 <qwer1304@hotmail.com>
2025-05-25 11:57:04 +0300

    more steps before updating worst

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -188,7 +188,7 @@ def _hparams(algorithm, dataset, random_seed):
             
     elif algorithm == "GLSD":
         _hparam('glsd_lambda', -1e1, lambda r: -1e1)#-10**r.uniform(-1, 5)) # FIX ME!
-        _hparam('update_worst_env_every_steps', 2, lambda r: 2)#-10**r.uniform(-1, 5))
+        _hparam('update_worst_env_every_steps', 4, lambda r: 4)#-10**r.uniform(-1, 5))
 
     if algorithm == "ADRMX":
         _hparam('cnt_lambda', 1.0, lambda r: r.choice([1.0]))
