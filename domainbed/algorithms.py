acc6420f8e4581d576503efa4844677c0925655f
qwer1304 <qwer1304@hotmail.com>
2025-05-26 17:30:34 +0300

    Try GFSD

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -2805,10 +2805,10 @@ class GLSD(ERM):
 
             # Previous code (Dai 2023) suggests relu
             if get_utility:
-                ux = torch.sum(nn.ReLU(eta - (F1x.unsqueeze(0)))*(mu.unsqueeze(1)), 0)
+                ux = torch.sum(F.relu(eta - (F1x.unsqueeze(0)))*(mu.unsqueeze(1)), dim=0)
                 return ux
             else:
-                ex = torch.mean(nn.ReLU(eta - F1x.unsqueeze(0)), 1)
+                ex = torch.mean(F.relu(eta - F1x.unsqueeze(0)), dim=1)
                 loss = torch.sum(ex*mu)
                 return loss
 
