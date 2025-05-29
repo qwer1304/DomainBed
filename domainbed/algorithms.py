041e31462b983f3493a24a8cab755bfe49f1250a
qwer1304 <qwer1304@hotmail.com>
2025-05-29 15:32:10 +0300

    Normalize losses

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -3102,7 +3102,7 @@ class GLSD(ERM):
         if pi_max < 1:
             worst_e_index = -worst_e_index
         # IMPORTANT!! train.py prints means of the values aggregated between prints, so worst_index becomes garbage!!!
-        return {'loss': loss.item(), 'n_loss_FSD': normalized['fsd'].item(), 'n_loss_SSD': normalized['ssd'.item(), 'nll': nll.mean().item(), 'worst_env': int(worst_e_index), }               
+        return {'loss': loss.item(), 'n_loss_FSD': normalized['fsd'].item(), 'n_loss_SSD': normalized['ssd'].item(), 'nll': nll.mean().item(), 'worst_env': int(worst_e_index), }               
 
 class GLSD_SSD(GLSD):
     """GLSD_SSD algorithm """
