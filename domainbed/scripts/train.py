83374c0f2fb80aeb820eb42f1dbb4de19a03ead4
qwer1304 <qwer1304@hotmail.com>
2025-06-01 13:57:50 +0300

    move alg and opt to cuda unconditionally

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -285,9 +285,6 @@ if __name__ == "__main__":
 
     algorithm.to(device)
     move_optimizer_to_device(algorithm.optimizer, device)
-    for name, param in algorithm.named_parameters():
-        print(f"{name}: {param.device}")
-
 
     train_minibatches_iterator = zip(*train_loaders)
     uda_minibatches_iterator = zip(*uda_loaders)
