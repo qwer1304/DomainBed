fix bug in calculation of F1

domainbed/algorithms.py
@@ -3320,7 +3320,7 @@ class GLSD(ERM):
             elif self.hparams["glsd_optimizer"] == "adam":
                 if self.glsd_after_load_state_count == self.hparams["glsd_after_load_state_count"] or \
                     self.glsd_after_load_state_count == 1 or \
-                    self.update_count == self.hparams["glsd_penalty_anneal_iters"]
+                    self.update_count == self.hparams["glsd_penalty_anneal_iters"]:
                         # Reset Adam (like IRM), because it doesn't like the sharp jump in
                         # gradient magnitudes that happens at this step.
                         self.optimizer = torch.optim.Adam(