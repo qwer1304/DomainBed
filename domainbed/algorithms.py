6f5775fedd2e3f65a6dc3c314c5ebb279b45824d
qwer1304 <qwer1304@hotmail.com>
2025-06-07 08:50:49 +0300

    add gradnorm

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -3421,6 +3421,7 @@ class GLSD(ERM):
             }
             # Final total loss
             nll_relu = F.relu(nll - self.hparams["glsd_nll_threshold_global"])
+            print(nll.item(), elf.hparams["glsd_nll_threshold_global"], nll_relu.item())
             loss = sum(signed_weighted_losses.values()) + self.hparams["glsd_nll_lambda"] * nll_relu
 
 
