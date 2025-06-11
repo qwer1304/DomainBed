447d9a2ae87babe57fa823420c4084e99a591192
qwer1304 <qwer1304@hotmail.com>
2025-06-11 13:41:18 +0300

    fix major error in regularization

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -3582,7 +3582,7 @@ class GLSD(ERM):
                     + self.hparams["glsd_gradnorm_lambda"] * loss_gradnorm
                 )
             else: # don't run gradnorm for several rounds
-                loss_weights = {"penalty": torch.tensor([0.001], device=device), "nll": torch.tensor([1.0], device=device)}
+                loss_weights = {"penalty": torch.tensor([1.0], device=device), "nll": torch.tensor([1.0], device=device)}
                 signed_weighted_losses = {
                     name: loss_signs[name] * loss_weights[name] * losses[name] for name in loss_weights
                 }
