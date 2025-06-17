d47f2fa8851d97f320ea4dd96f41613c734d0316
qwer1304 <qwer1304@hotmail.com>
2025-06-17 17:44:47 +0300

    fix sweep

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -377,7 +377,10 @@ if __name__ == "__main__":
 
             epochs_path = os.path.join(args.output_dir, 'results.jsonl')
             with open(epochs_path, 'a') as f:
-                f.write(json.dumps(results, sort_keys=True) + "\n")
+                try:
+                    f.write(json.dumps(results, sort_keys=True) + "\n")
+                except TypeError as err:
+                    print('error:', err, 'results:', results)
 
             algorithm_dict = algorithm.state_dict()
             start_step = step + 1
