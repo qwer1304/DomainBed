506b5c753a89a6aabd2cd12247dea93899eea9e6
qwer1304 <qwer1304@hotmail.com>
2025-06-17 19:30:24 +0300

    fix sweep

--- domainbed/scripts/sweep.py
+++ domainbed/scripts/sweep.py
@@ -33,9 +33,9 @@ class Job:
     DONE = 'Done'
 
     def __init__(self, train_args, sweep_output_dir):
-        train_args_for_hash = train_args
+        train_args_for_hash = copy.deepcopy(train_args)
         if 'load_from_checkpoint' in train_args_for_hash:
-            del train_args_for_hash['load_from_checkpoint'].deepcopy()
+            del train_args_for_hash['load_from_checkpoint']
         args_str = json.dumps(train_args_for_hash, sort_keys=True)
         args_hash = hashlib.md5(args_str.encode('utf-8')).hexdigest()
         self.output_dir = os.path.join(sweep_output_dir, args_hash)
