a8fb706a47f5541be53c33a2b103cf0e14372e7b
qwer1304 <qwer1304@hotmail.com>
2025-06-26 19:43:05 +0300

    model selection regularizer

--- domainbed/model_selection_regularize.py
+++ domainbed/model_selection_regularize.py
@@ -83,12 +83,10 @@ def regularize_model_selection(algorithm, evals, num_classes, device):
             kde_result = torch.stack(kde_result_list, dim=0) # (N,M,D)
             # (N,N,D)     (N,1,M,D)            (1,N,M,D)
             TV = (kde_result.unsqueeze(1) - kde_result.unsqueeze(0)).abs().sum(2)
-            print(TV.size())
             
             TV_avail_list = [0]*N
             for i in range(ind_split.size(0)): # N
-                TTV = TV[ind_split[i],ind_split[i]]
-                print(TTV.size())
+                TTV = TV[ind_split[i]][:,ind_split[i]]
                 for j in range(ind_split.size(1)):
                     mask = torch.arange(ind_split.size(1)) != j  # exclude index j
                     sub = TTV[mask][:, mask]      # (k-1, k-1, D)
